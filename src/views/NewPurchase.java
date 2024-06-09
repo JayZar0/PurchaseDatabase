e8704124d8f554a804efdeace9091002fb88b297
lazaro4958 <lazaro4958@saskpolytech.ca>
2024-06-09 02:55:39 -0600

    Made some structural changes to the repository.

--- src/views/NewPurchase.java
+++ src/views/NewPurchase.java
@@ -1,5 +1,35 @@
 package views;
 
-public class NewPurchase
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * This class will be used to control the new purchase screen when the user
+ * wants to add a purchase into the database. It will prompt the type of purchase
+ * and the amount they paid.
+ * @author johne
+ * @version 1.0
+ */
+public class NewPurchase implements Initializable
 {
+    @FXML
+    private TextField txtAmount;
+    @FXML
+    private ComboBox<String> cboType;
+    @FXML
+    private Button cmdClear;
+    @FXML
+    private Button cmdConfirm;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        cboType.getItems().addAll("Cash", "Debit", "Mastercard", "Visa", "American Express");
+    }
 }
