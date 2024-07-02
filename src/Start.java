1cf661890fbe9a8367148c427f6d4a5220445a75
lazaro4958 <lazaro4958@saskpolytech.ca>
2024-07-02 17:26:23 -0600

    added some classes and finished screens.

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.net.URI;
 
 /**
  * This is the main start method that will launch the application and allow
@@ -15,11 +21,18 @@ public class Start extends Application
     }
 
     @Override
-    public void start(Stage stage)
+    public void start(Stage stage) throws Exception
     {
         // pull all the screens and place them on to the window
+        FXMLLoader loader = new FXMLLoader(Start.class.getResource("MainScreen.fxml"));
+        Pane mainPane = loader.load();
+
         // set scene
+        stage.setScene(new Scene(mainPane));
+        stage.setTitle("Transaction Analysis");
+
         // stage show
+        stage.show();
 
         //start all the network and database dependencies here
     }
--- src/views/MainScreen.java
+++ src/views/MainScreen.java
@@ -1,7 +1,15 @@
 package views;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,12 +22,34 @@ import java.util.ResourceBundle;
  */
 public class MainScreen implements Initializable
 {
-    //FXML Variables
-    //TODO add the variables and the handlers for all the sections in the application
+    Stage stage = new Stage();
 
+    //FXML Variables
+    @FXML
+    MenuItem mnuAdd;
+    @FXML
+    MenuItem mnuDelete;
+
+    //TODO add the variables and the handlers for all the sections in the application
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        mnuAdd.setOnAction(e -> {
+            try
+            {
+                FXMLLoader loader = new FXMLLoader(MainScreen.class.getResource("NewPurchaseForm.fxml"));
+                Pane addPane = loader.load();
 
+                stage.setScene(new Scene(addPane));
+                stage.setTitle("Transaction form");
+                stage.showAndWait();
+            }
+            catch (IOException ex)
+            {
+                Alert obAlert = new Alert(Alert.AlertType.ERROR);
+                obAlert.setTitle("Error On Adding Purchase");
+                obAlert.setContentText("The program had an error while pulling up the transaction addition form");
+            }
+        });
     }
 }
